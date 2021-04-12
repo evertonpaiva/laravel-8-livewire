271ce5bb72fb95f3dc5db7f1bd754f4aed48c510
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-04-12 16:41:12 -0300

    Criação de usuário através dos dados obtidos via biblioteca de integração da UFVJM.

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -7,9 +7,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Jetstream\Jetstream;
-use GraphqlClient\GraphqlRequest\AuthGraphqlRequest;
-use Illuminate\Validation\ValidationException;
-use GraphqlClient\GraphqlRequest\Common\PessoaGraphqlRequest;
+use App\Actions\Ufvjm\AuthContaInstitucional;
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -34,7 +32,8 @@ class CreateNewUser implements CreatesNewUsers
         $loginInfo->containstitucional = $input['containstitucional'];
         $loginInfo->password = $input['password'];
 
-        $dados = $this->logarContaInstitucional($loginInfo);
+        $authContaInstitucional = new AuthContaInstitucional();
+        $dados = $authContaInstitucional->logarContaInstitucional($loginInfo);
 
         return User::create([
             'nome' => $dados['nome'],
@@ -43,63 +42,6 @@ class CreateNewUser implements CreatesNewUsers
             'cpf' => $dados['cpf'],
             'idpessoa' => $dados['idpessoa'],
             'password' => Hash::make(''),
-        ]);
-    }
-
-    /**
-     * Tenta realizar autenticação na Conta Institucional da UFVJM
-     * Caso tenha sucesso, retorna alguma informações do usuário logado
-     *
-     * @param object $request
-     * @return array
-     * @throws ValidationException
-     */
-    private function logarContaInstitucional(object $request)
-    {
-        try {
-            // Carrega a classe de autenticação
-            $authGraphqlRequest = new AuthGraphqlRequest();
-
-            // Tenta realizar o login na Conta Institucional
-            $authGraphqlRequest->loginContaInstitucional($request);
-
-            // Recupera as informações do usuário logado
-            $userInfo = $authGraphqlRequest->usuarioLogadoInfo();
-
-            $idpessoa = $userInfo->idpessoa;
-
-            // Carrega a classe de pessoa
-            $pessoaGraphqlRequest = new PessoaGraphqlRequest();
-
-            // Recupera informações de pessoa por código
-            $pessoa =
-                $pessoaGraphqlRequest
-                    ->queryGetById($idpessoa)
-                    ->getResults();
-
-            // Completa os dados de usuário, para cadastrá-lo
-            $dados = [];
-            $dados['nome'] = $userInfo->nome;
-            $dados['cpf'] = $userInfo->cpf;
-            $dados['idpessoa'] = $pessoa->idpessoa;
-
-            return $dados;
-        } catch (\Exception $e) {
-            $errorMessage = $e->getMessage();
-            $this->sendFailedRegisterResponse($request, $errorMessage);
-        }
-    }
-
-    /**
-     * Trata as messagens de erro em caso de falha
-     * @param object $request
-     * @param String $message
-     * @throws ValidationException
-     */
-    protected function sendFailedRegisterResponse(object $request, String $message)
-    {
-        throw ValidationException::withMessages([
-            'containstitucional' => [$message],
         ]);
     }
 }
