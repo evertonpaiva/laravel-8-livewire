e016854ef4efa75d45c368bf5e204073295775ea
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-04-12 16:40:22 -0300

    Implementando login de usuário através da biblioteca de integração da UFVJM.

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -6,11 +6,13 @@ use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
+use App\Actions\Ufvjm\AuthContaInstitucional;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
+use App\Models\User;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -42,6 +44,20 @@ class FortifyServiceProvider extends ServiceProvider
 
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        });
+
+        Fortify::authenticateUsing(function (Request $request) {
+            $loginInfo = new \stdClass();
+            $loginInfo->containstitucional = $request->containstitucional;
+            $loginInfo->password = $request->password;
+
+            $authContaInstitucional = new AuthContaInstitucional();
+            $authContaInstitucional->logarContaInstitucional($loginInfo);
+
+            $user = User::where('containstitucional', $loginInfo->containstitucional)->first();
+
+            if ($user) return $user;
+            else false;
         });
     }
 }
