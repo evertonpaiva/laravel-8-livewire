abfd83691828f8848751a3b11a78645a39e305a4
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-04-12 13:47:48 -0300

    Atualizando view de formulário de registro de novo usuário no sistema.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,29 +4,31 @@
             <x-jet-authentication-card-logo />
         </x-slot>
 
+        <div class="mt-4">
+            <h1>Bem-vindo ao {{ env('APP_NAME') }}</h1>
+            <h3 class="p-1 text-gray-700">
+                Primeira vez acessando o {{ env('APP_NAME') }}? Por favor, informe seu e-mail preferencial,
+                sua conta institucional (sem @ufvjm.edu.br) e sua senha (a mesma utilizada no e-Campus).</h3>
+        </div>
+
         <x-jet-validation-errors class="mb-4" />
 
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
-            <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <div class="mt-4">
+                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-jet-label for="containstitucional" value="{{ __('Conta Institucional') }}" />
+                <x-jet-input id="containstitucional" class="block mt-1 w-full" type="text" name="containstitucional" :value="old('name')" required autocomplete="containstitucional" />
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
             @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
@@ -48,11 +50,11 @@
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    {{ __('Já é cadastrado?') }}
                 </a>
 
                 <x-jet-button class="ml-4">
-                    {{ __('Register') }}
+                    {{ __('Cadastrar') }}
                 </x-jet-button>
             </div>
         </form>
