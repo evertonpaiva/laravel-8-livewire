02823412830c7b7d945391bd09f7d2231a7ec92e
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-04-12 16:37:53 -0300

    Desabilitando functionalidades de resetarPassword e autenticação em 2 fatores.

--- config/fortify.php
+++ config/fortify.php
@@ -46,7 +46,7 @@ return [
     |
     */
 
-    'username' => 'email',
+    'username' => 'containstitucional',
 
     'email' => 'email',
 
@@ -133,13 +133,13 @@ return [
 
     'features' => [
         Features::registration(),
-        Features::resetPasswords(),
+        // Features::resetPasswords(),
         // Features::emailVerification(),
         Features::updateProfileInformation(),
-        Features::updatePasswords(),
-        Features::twoFactorAuthentication([
-            'confirmPassword' => true,
-        ]),
+        // Features::updatePasswords(),
+        // Features::twoFactorAuthentication([
+        //    'confirmPassword' => true,
+        //]),
     ],
 
 ];
