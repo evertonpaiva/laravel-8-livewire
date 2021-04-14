8c837c8ec6023fc4c696977e19e7885bd264eb4d
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-04-14 17:58:15 -0300

    Desativando output tty

--- phpcs.sh
+++ phpcs.sh
@@ -3,7 +3,7 @@
 CONTAINER_WEB_NAME=laravel_dds_web
 
 echo -e "\nValidando Padrao de codificacao"
-docker exec -it ${CONTAINER_WEB_NAME} ./vendor/bin/phpcs
+docker exec ${CONTAINER_WEB_NAME} ./vendor/bin/phpcs
 RETORNO=$?
 
 exit $RETORNO
