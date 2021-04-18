10ddea37817e8b656b71a6c21a958da8c99ac7bc
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-04-18 10:42:25 -0300

    Criando função para gerar usuários fake e definir perfil aleatório

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,13 +14,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
         $this->call([
             NavigationMenuSeeder::class,
             RoleTableSeeder::class,
             PermissionTableSeeder::class,
             RolePermissionsTableSeeder::class,
         ]);
+    }
+
+    /**
+     * Criando usuarios e atribuindo um perfil aleatoria e eles
+     * @throws \Exception
+     */
+    private function createFakeUsers()
+    {
+        // numero de usuarios
+        $newUsers = 100;
+
+        // loop para criar usuarios
+        for($i = 0; $i < $newUsers; $i++){
+            // criando um usuario
+            $user = \App\Models\User::factory(1)->create()->first();
+
+            // numero de perfis - 1, ajuste pois o vetor se inicia em 0
+            $numberOfRoles = count(User::userRoleList()) - 1;
+
+            // escolher aleatoriamente um indice para o vetor de nome de perfis
+            $randomInt = random_int(0, $numberOfRoles);
+            $roleName = User::userRoleList()[$randomInt];
+
+            // atribuindo um perfil para o usuario criado
+            $user->assignRole($roleName);
+        }
     }
 }
