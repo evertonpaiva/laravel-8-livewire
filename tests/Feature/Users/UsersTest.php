a26f75ec28f142464acc86a0a0cb289da547c6c0
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-04-20 17:48:15 -0300

    Atualizando testes de formulário de usuário. Adicionando e removendo perfis.

--- tests/Feature/Users/UsersTest.php
+++ tests/Feature/Users/UsersTest.php
@@ -6,6 +6,7 @@ use App\Http\Livewire\Users;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Livewire;
+use Spatie\Permission\Models\Role;
 use Tests\TestCase;
 use Illuminate\Support\Arr;
 
@@ -89,6 +90,62 @@ class UsersTest extends TestCase
         $this->assertDatabaseHas(
             'users',
             Arr::except($user->getAttributes(), ['updated_at'])
+        );
+    }
+
+    public function testAdminUserCanAddRole()
+    {
+        $userAdmin = User::factory()->create();
+        $userAdmin->assignRole('Admin');
+        $this->actingAs($userAdmin);
+
+        $user = User::factory()->create();
+
+        // perfil a adicionar
+        $roleName = 'Usuário';
+
+        Livewire::test(Users::class)
+            ->call('updateShowModal', $user->id)
+            ->set('roleNameAdd', $roleName)
+            ->call('update');
+
+        // o perfil adicionado esta no vetor de perfis do usuario?
+        $this->assertTrue(
+            in_array(
+                $roleName,
+                $user->fresh()
+                    ->getRoleNames()
+                    ->toArray()
+            )
+        );
+    }
+
+    public function testAdminUserCanRemoveRole()
+    {
+        $userAdmin = User::factory()->create();
+        $userAdmin->assignRole('Admin');
+        $this->actingAs($userAdmin);
+
+        // perfil a remover
+        $roleName = 'Usuário';
+
+        $user = User::factory()->create();
+        $user->assignRole($roleName);
+
+        Livewire::test(Users::class)
+            ->call('updateShowModal', $user->id)
+            ->call('update')
+            ->call('deleteRoleShowModal', $user->id, $roleName)
+            ->call('deleteRole');
+
+        // o perfil adicionado nao esta no vetor de perfis do usuario?
+        $this->assertTrue(
+            !in_array(
+                $roleName,
+                $user->fresh()
+                    ->getRoleNames()
+                    ->toArray()
+            )
         );
     }
 
