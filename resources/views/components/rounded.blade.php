83477fa75a8afe685ca78bd4876f0356d96f9c3d
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-04-20 11:30:33 -0300

    Criando componente para span colorido com bordas arredondadas

---
+++ resources/views/components/rounded.blade.php
@@ -0,0 +1,3 @@
+<span class="mr-2 px-2 py-1 font-semibold leading-tight text-{{ $color }}-700 bg-{{ $color }}-100 rounded-full dark:bg-{{ $color }}-700 dark:text-{{ $color }}-100">
+    {{ $slot }}
+</span>
