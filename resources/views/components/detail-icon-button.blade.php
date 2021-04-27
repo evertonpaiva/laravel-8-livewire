09baa88b20798075841adf5b773dedab4a25ad41
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-04-27 08:04:44 -0300

    Issue #10. Criando botão detail.

---
+++ resources/views/components/detail-icon-button.blade.php
@@ -0,0 +1,3 @@
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-purple']) }}>
+    <i class="fas fa-search"></i>
+</button>
