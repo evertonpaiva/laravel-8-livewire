fdfdcf70501b47b3b3fc1b93a3b113a32332991b
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-04-29 20:26:24 -0300

    Issue #12. Criando função para mapear os ids com os enums da integração.

--- app/Models/Modalidade.php
+++ app/Models/Modalidade.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GraphqlClient\GraphqlRequest\Ensino\ModalidadeGraphqlRequest;
 
 class Modalidade extends Model
 {
@@ -14,4 +15,16 @@ class Modalidade extends Model
     public $incrementing = false;
 
     protected $fillable = [ 'idmodalidade', 'modalidade'];
+
+    public static function idModalidadeToEnum($idModalidade)
+    {
+        switch ($idModalidade) {
+            case 1:
+                return ModalidadeGraphqlRequest::PRESENCIAL;
+            case 2:
+                return ModalidadeGraphqlRequest::EAD;
+            case 3:
+                return ModalidadeGraphqlRequest::LEC;
+        }
+    }
 }
