405bca6d425ef78d34bac486688711059120ac37
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-05-01 14:05:04 -0300

    Issue #12. Criando função pra login nos testes, para testar a integração.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,25 @@
 
 namespace Tests;
 
+use App\Actions\Ufvjm\AuthContaInstitucional;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Realiza login na Integracao
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function loginIntegracao()
+    {
+        $loginInfo = new \stdClass();
+
+        $loginInfo->containstitucional = env('LDAP_USERNAME');
+        $loginInfo->password = env('LDAP_PASSWORD');
+
+        $authContaInstitucional = new AuthContaInstitucional();
+        $authContaInstitucional->logarContaInstitucional($loginInfo);
+    }
 }
